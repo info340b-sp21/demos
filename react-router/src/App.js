7444d01ea6a616ab8767c67db214ec8b82361030
Tim Carlson <timca@dashplatform.com>
2021-05-29 16:04:18 -0700

    lots

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import { Button } from 'reactstrap';
 
 // // Slide 15 Browser Router
-// import {BrowserRouter, Route} from 'react-router-dom';
+// import { BrowserRouter, Route } from 'react-router-dom';
 
 // // slide 18 switch
 // import { BrowserRouter, Route, Switch } from 'react-router-dom';
@@ -18,7 +18,7 @@ import { BrowserRouter, Route, Switch, NavLink,useParams, Redirect } from 'react
 function App(props) {
 
   const [posts, setPosts] = useState(props.initialPosts);
-  
+
   const addPost = (text, date) => {
     console.log("adding post");
     let postsCopy = { ...posts } //make copy
@@ -59,203 +59,203 @@ function App(props) {
   //   </div>
   // );
 
-// // Slide 15 BrowserRouter
-//   return (
-//     <BrowserRouter>
-//     <div className="container">
-//       <h1>My Blog</h1>
-//       <nav>
-//         <ul className="nav">
-//           <li>
-//             <a href='/' className="nav-link">Home</a>
-//           </li>
-//           <li>
-//             <a href='/about' className="nav-link">About</a>
-//           </li>
-//           <li>
-//             <a href='/blog' className="nav-link">Blog</a>
-//           </li>
-//           {postLinks}
-//         </ul>
-//       </nav>
-//       <NewPostPage postCallback={addPost} />
-//       <AboutPage />
-//       <BlogPostList posts={posts} />
-//     </div>
-//     </BrowserRouter>
-//   );
+  // // Slide 15 BrowserRouter
+  //   return (
+  //     <BrowserRouter>
+  //     <div className="container">
+  //       <h1>My Blog</h1>
+  //       <nav>
+  //         <ul className="nav">
+  //           <li>
+  //             <a href='/' className="nav-link">Home</a>
+  //           </li>
+  //           <li>
+  //             <a href='/about' className="nav-link">About</a>
+  //           </li>
+  //           <li>
+  //             <a href='/blog' className="nav-link">Blog</a>
+  //           </li>
+  //           {postLinks}
+  //         </ul>
+  //       </nav>
+  //       <NewPostPage postCallback={addPost} />
+  //       <AboutPage />
+  //       <BlogPostList posts={posts} />
+  //     </div>
+  //     </BrowserRouter>
+  //   );
 
-// // Slide 17 Route
-//   return (
-//     <BrowserRouter>
-//     <div className="container">
-//       <h1>My Blog</h1>
-//       <nav>
-//         <ul className="nav">
-//           <li>
-//             <a href='/' className="nav-link">Home</a>
-//           </li>
-//           <li>
-//             <a href='/about' className="nav-link">About</a>
-//           </li>
-//           <li>
-//             <a href='/blog' className="nav-link">Blog</a>
-//           </li>
-//           {postLinks}
-//         </ul>
-//       </nav>
-//       <Route exact path="/"> {/* with exact,  it has to begin and end with '/' */}
-//             <WelcomePage />
-//           </Route>
-//           <Route path="/post"> {/* this is basically an if statement*/}
-//             <NewPostPage postCallback={addPost} />
-//           </Route>
-//           <Route path="/about">
-//             <AboutPage />
-//           </Route>
-//           <Route exact path="/blog"> {/* with exact, it will only show '/blog' */}
-//             <BlogPostList posts={posts} />
-//           </Route>
-//           <Route path="/blog/posts/:postDate">
-//             <BlogPost />
-//           </Route>
-//     </div>
-//     </BrowserRouter>
-//   );
+  // // Slide 17 Route
+  // return (
+  //   <BrowserRouter>
+  //     <div className="container">
+  //       <h1>My Blog</h1>
+  //       <nav>
+  //         <ul className="nav">
+  //           <li>
+  //             <a href='/' className="nav-link">Home</a>
+  //           </li>
+  //           <li>
+  //             <a href='/about' className="nav-link">About</a>
+  //           </li>
+  //           <li>
+  //             <a href='/blog' className="nav-link">Blog</a>
+  //           </li>
+  //           {postLinks}
+  //         </ul>
+  //       </nav>
+  //       <Route exact path="/"> {/* with exact,  it has to begin and end with '/' */}
+  //         <WelcomePage />
+  //       </Route>
+  //       <Route path="/post"> {/* this is basically an if statement*/}
+  //         <NewPostPage postCallback={addPost} />
+  //       </Route>
+  //       <Route path="/about">
+  //         <AboutPage />
+  //       </Route>
+  //       <Route exact path="/blog"> {/* with exact, it will only show '/blog' */}
+  //         <BlogPostList posts={posts} />
+  //       </Route>
+  //       <Route path="/blog/posts/:postDate">
+  //         <BlogPost />
+  //       </Route>
+  //     </div>
+  //   </BrowserRouter>
+  // );
 
-// // Slide 18 Switch
-// return (
-//   <BrowserRouter>
-//   <div className="container">
-//     <h1>My Blog</h1>
-//     <nav>
-//       <ul className="nav">
-//         <li>
-//           <a href='/' className="nav-link">Home</a>
-//         </li>
-//         <li>
-//           <a href='/about' className="nav-link">About</a>
-//         </li>
-//         <li>
-//           <a href='/blog' className="nav-link">Blog</a>
-//         </li>
-//         {postLinks}
-//       </ul>
-//     </nav>
-//     <Switch>
-//           <Route exact path="/"> {/* with exact, now it has to begin and end with '/' */}
-//             <WelcomePage />
-//           </Route>
-//           <Route path="/post"> {/* this is basically an if statement*/}
-//             <NewPostPage postCallback={addPost} />
-//           </Route>
-//           <Route path="/about">
-//             <AboutPage />
-//           </Route>
-//           <Route exact path="/blog">
-//             <BlogPostList posts={posts} />
-//           </Route>
-//           <Route path="/blog/posts/:postDate">
-//             <BlogPost />
-//           </Route>
+  // // Slide 18 Switch
+  // return (
+  //   <BrowserRouter>
+  //   <div className="container">
+  //     <h1>My Blog</h1>
+  //     <nav>
+  //       <ul className="nav">
+  //         <li>
+  //           <a href='/' className="nav-link">Home</a>
+  //         </li>
+  //         <li>
+  //           <a href='/about' className="nav-link">About</a>
+  //         </li>
+  //         <li>
+  //           <a href='/blog' className="nav-link">Blog</a>
+  //         </li>
+  //         {postLinks}
+  //       </ul>
+  //     </nav>
+  //     <Switch>
+  //           <Route exact path="/"> {/* with exact, now it has to begin and end with '/' */}
+  //             <WelcomePage />
+  //           </Route>
+  //           <Route path="/post"> {/* this is basically an if statement*/}
+  //             <NewPostPage postCallback={addPost} />
+  //           </Route>
+  //           <Route path="/about">
+  //             <AboutPage />
+  //           </Route>
+  //           <Route exact path="/blog">
+  //             <BlogPostList posts={posts} />
+  //           </Route>
+  //           <Route path="/blog/posts/:postDate">
+  //             <BlogPost />
+  //           </Route>
 
-//         </Switch>
-//   </div>
-//   </BrowserRouter>
-// );
+  //         </Switch>
+  //   </div>
+  //   </BrowserRouter>
+  // );
 
-// // Slide 19 Link
-// return (
-//   <BrowserRouter>
-//   <div className="container">
-//     <h1>My Blog</h1>
-//     <nav>
-//       <ul className="nav">
-//         <li>
-//           <NavLink exact to='/' className="nav-link" activeClassName="myActiveLink">Home</NavLink>
-//           {/* <NavLink to='/' className="nav-link" activeClassName="myActiveLink">Home</NavLink> */}
-//         </li>
-//         <li>
-//           <NavLink to='/about' className="nav-link" activeClassName="myActiveLink">About</NavLink>
-//         </li>
-//         <li>
-//           <NavLink exact to='/blog' className="nav-link" activeClassName="myActiveLink">Blog</NavLink>
-//           {/* <NavLink to='/blog' className="nav-link" activeClassName="myActiveLink">Blog</NavLink> */}
-//         </li>
-//         {postLinks}
-//       </ul>
-//     </nav>
-//     <Switch>
-//       <Route exact path="/"> {/* with exact, now it has to begin and end with '/' */}
-//         <WelcomePage />
-//       </Route>
-//       <Route path="/post"> {/* this is basically an if statement*/}
-//         <NewPostPage postCallback={addPost} />
-//       </Route>
-//       <Route path="/about">
-//         <AboutPage />
-//       </Route>
-//       <Route exact path="/blog">
-//         <BlogPostList posts={posts} />
-//       </Route>
-//       <Route path="/blog/posts/:postDate">
-//         <BlogPost />
-//       </Route>
+  // // Slide 19 Link
+  // return (
+  //   <BrowserRouter>
+  //   <div className="container">
+  //     <h1>My Blog</h1>
+  //     <nav>
+  //       <ul className="nav">
+  //         <li>
+  //           <NavLink exact to='/' className="nav-link" activeClassName="myActiveLink">Home</NavLink>
+  //           {/* <NavLink to='/' className="nav-link" activeClassName="myActiveLink">Home</NavLink> */}
+  //         </li>
+  //         <li>
+  //           <NavLink to='/about' className="nav-link" activeClassName="myActiveLink">About</NavLink>
+  //         </li>
+  //         <li>
+  //           <NavLink exact to='/blog' className="nav-link" activeClassName="myActiveLink">Blog</NavLink>
+  //           {/* <NavLink to='/blog' className="nav-link" activeClassName="myActiveLink">Blog</NavLink> */}
+  //         </li>
+  //         {postLinks}
+  //       </ul>
+  //     </nav>
+  //     <Switch>
+  //       <Route exact path="/"> {/* with exact, now it has to begin and end with '/' */}
+  //         <WelcomePage />
+  //       </Route>
+  //       <Route path="/post"> {/* this is basically an if statement*/}
+  //         <NewPostPage postCallback={addPost} />
+  //       </Route>
+  //       <Route path="/about">
+  //         <AboutPage />
+  //       </Route>
+  //       <Route exact path="/blog">
+  //         <BlogPostList posts={posts} />
+  //       </Route>
+  //       <Route path="/blog/posts/:postDate">
+  //         <BlogPost />
+  //       </Route>
 
-//     </Switch>
+  //     </Switch>
 
-//   </div>
-// </BrowserRouter>
-// );
+  //   </div>
+  // </BrowserRouter>
+  // );
 
-// Slide 23 Redirect
-return (
-  <BrowserRouter>
-  <div className="container">
-    <h1>My Blog</h1>
-    <nav>
-      <ul className="nav">
-        <li>
-          {/* <NavLink exact to='/' className="nav-link" activeClassName="myActiveLink">Home</NavLink> */}
-          <NavLink to='/' className="nav-link" activeClassName="myActiveLink">Home</NavLink>
-        </li>
-        <li>
-          <NavLink to='/about' className="nav-link" activeClassName="myActiveLink">About</NavLink>
-        </li>
-        <li>
-          {/* <NavLink exact to='/blog' className="nav-link" activeClassName="myActiveLink">Blog</NavLink> */}
-          <NavLink to='/blog' className="nav-link" activeClassName="myActiveLink">Blog</NavLink>
-        </li>
-        {postLinks}
-      </ul>
-    </nav>
-    <Switch>
-          <Route exact path="/"> {/* with exact, now it has to begin and end with '/' */}
-            <WelcomePage />
-          </Route>
-          <Route path="/post"> {/* this is basically an if statement*/}
-            <NewPostPage postCallback={addPost} />
-          </Route>
-          <Route path="/about">
-            <AboutPage />
-          </Route>
-          <Route exact path="/blog">
-            <BlogPostList posts={posts} />
-          </Route>
-          <Route path="/blog/posts/:postDate">
-            <BlogPost />
-          </Route>
-          <Route path="/">
-            {/* match to everything */}
-            {/* <ErrorPage/> */}
-            <Redirect to="/" />
-          </Route>
+  // Slide 23 Redirect
+  return (
+    <BrowserRouter>
+    <div className="container">
+      <h1>My Blog</h1>
+      <nav>
+        <ul className="nav">
+          <li>
+            {/* <NavLink exact to='/' className="nav-link" activeClassName="myActiveLink">Home</NavLink> */}
+            <NavLink exact to='/' className="nav-link" activeClassName="myActiveLink">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to='/about' className="nav-link" activeClassName="myActiveLink">About</NavLink>
+          </li>
+          <li>
+            {/* <NavLink exact to='/blog' className="nav-link" activeClassName="myActiveLink">Blog</NavLink> */}
+            <NavLink exact to='/blog' className="nav-link" activeClassName="myActiveLink">Blog</NavLink>
+          </li>
+          {postLinks}
+        </ul>
+      </nav>
+      <Switch>
+            <Route exact path="/"> {/* with exact, now it has to begin and end with '/' */}
+              <WelcomePage />
+            </Route>
+            <Route path="/post"> {/* this is basically an if statement*/}
+              <NewPostPage postCallback={addPost} />
+            </Route>
+            <Route path="/about">
+              <AboutPage />
+            </Route>
+            <Route exact path="/blog">
+              <BlogPostList posts={posts} />
+            </Route>
+            <Route path="/blog/posts/:postDate">
+              <BlogPost />
+            </Route>
+            <Route path="/">
+              {/* match to everything */}
+              {/* <ErrorPage/> */}
+              <Redirect to="/" />
+            </Route>
 
-        </Switch>
+          </Switch>
 
-  </div>
-</BrowserRouter>
-);
+    </div>
+  </BrowserRouter>
+  );
 
 }
 
@@ -295,7 +295,7 @@ function NewPostPage(props) {
       />
 
       <Button color="danger" className="mb-3" onClick={handleClick}>
-      Post!
+        Post!
       </Button>
 
       {/* <button className="btn btn-primary mb-3" onClick={handleClick}>
@@ -358,6 +358,7 @@ function BlogPost(props) {
   console.log(urlParams)
   let date = props.postDate;
   let post = props.post;
+  
 
   if (props.date === undefined) {
 
